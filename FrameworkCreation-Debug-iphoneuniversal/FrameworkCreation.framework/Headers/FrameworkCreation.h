c51a3d06e3a5b7bd323a9e8e101cfc7a413c1dde
Jeevitha S <apple@JeevithaMac.local>
2017-06-13 17:09:12 +0530

    Added shared scheme

---
+++ FrameworkCreation-Debug-iphoneuniversal/FrameworkCreation.framework/Headers/FrameworkCreation.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkCreation.h
+//  FrameworkCreation
+//
+//  Created by Jeevitha S. on 13/06/17.
+//  Copyright © 2017 ndot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkCreation.
+FOUNDATION_EXPORT double FrameworkCreationVersionNumber;
+
+//! Project version string for FrameworkCreation.
+FOUNDATION_EXPORT const unsigned char FrameworkCreationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkCreation/PublicHeader.h>
+
+
---
+++ Integration-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Integration-Bridging-Header.h
+//  Integration
+//
+//  Created by Jeevitha S. on 13/06/17.
+//  Copyright © 2017 ndot. All rights reserved.
+//
+
+#ifndef Integration_Bridging_Header_h
+#define Integration_Bridging_Header_h
+
+
+#endif /* Integration_Bridging_Header_h */
